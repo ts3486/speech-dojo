dbb55cab814e595f64d166faf7d4b3d92ddccb3e
ts3486 <taos3486@gmail.com>
2026-02-23 09:15:13 +0900

    stop tracking .next files

--- frontend/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"pages/[[...slug]].tsx -> ../src/App\":{\"id\":\"pages/[[...slug]].tsx -> ../src/App\",\"files\":[\"static/chunks/_pages-dir-browser_src_App_tsx.js\"]}}"
